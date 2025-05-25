c66034d669ac3301d6381626c9e4f3114b9129dc
abhigyan9 <105762331+abhigyan9@users.noreply.github.com>
2025-05-25 18:27:34 +0000

    Deploying to gh-pages from @ abhigyan9/abhigyan9.github.io@8358663c2e16cc0c3b1e66ab998958a7927cbe4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -385,11 +385,6 @@ ninja.data = [{
           description: "Proof-of-concept simulation of a quadcopter autonomously landing on a moving carrier ship using analytical trajectory planning and adaptive control.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{
         id: 'social-email',
         title: 'email',
